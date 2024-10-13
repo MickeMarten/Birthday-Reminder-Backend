b012b001f72d4d849b784b05d02a6cec7334afb9
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-10-13 23:58:16 +0200

    bot < v

--- assemble-bot.ts
+++ assemble-bot.ts
@@ -1,9 +1,9 @@
-import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.27.0/mod.ts"
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const env = config();
 const TOKEN = env.BOTTOKEN;
-console.log('tokej',TOKEN);
+console.log('tokem',TOKEN);
 if (!TOKEN) throw new Error("Något har hänt med bot-token, fråga Botfather");
 
 const bot = new Bot(TOKEN);
