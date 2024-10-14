60c7c00f8542726505024c4d98686f5087828f3b
MickeMarten <Micke.Marten123@gmail.com>
2024-10-14 23:22:37 +0200

    test2

--- assemble-bot.ts
+++ assemble-bot.ts
@@ -1,11 +1,12 @@
-import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { Bot } from 'https://deno.land/x/grammy@v1.30.0/mod.ts';
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 export function assembleBot() {
   const env = config();
   const TOKEN = env.BOTTOKEN;
-  if (!TOKEN) throw new Error("Något har hänt med bot-token, fråga Botfather");
+  if (!TOKEN) throw new Error('Något har hänt med bot-token, fråga Botfather');
   const chatID: string = env.CHATID;
+
   const bot = new Bot(TOKEN);
 
   return { bot, chatID };
